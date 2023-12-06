6b2afceeace69c9f851f05f954ed05f75d1e7cdd
kilokomuli <kilokoemma175@gmail.com>
2023-12-06 03:55:37 -0800

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newlist = [replace if  x == search else x for x in my_list]
+    newlist = [replace if x == search else x for x in my_list]
     return newlist
