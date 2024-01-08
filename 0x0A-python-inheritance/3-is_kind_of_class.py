465387f2d1427aa38a2785549de303a0453187d7
kilokomuli <kilokoemma175@gmail.com>
2024-01-08 11:56:00 -0800

    documenting

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" defines a function is_kind_of_class"""
+
+
 def is_kind_of_class(obj, a_class):
     """ Returns True if the object is an instance of,
     or if the onject is an instance of a class that inherited
