ad82caebc155ea60a594aae2e5bd199ee510de9e
kilokomuli <kilokoemma175@gmail.com>
2024-01-08 23:14:23 -0800

    correction 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
-""" Define class BaseGeometry"""
+""" Define class BaseGeometry with an unimplemendted area"""
 
 
-class BaseGeometry:
-    """ BaseGeometry class witha n unimplemented area
-    method"""
-
-    def area(self):
-        """Raises an exception with the message are() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates the value to be an integer and greaterhan 0
-        Args:
-            name: a string representing name of the value
-            Value: the value to be validated
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if the value is less then or equal to 0"""
-        if not isinstance(value, int):
-            raise TypeError("name must be an integer")
-        if value <= 0:
-            raise ValueError("name must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
