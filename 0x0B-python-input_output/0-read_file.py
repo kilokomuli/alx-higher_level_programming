e0dd12547f526d8ad2a75737f51f2d7a4319a863
kilokomuli <kilokoemma175@gmail.com>
2024-01-09 04:14:10 -0800

    removed whitespaces

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
         """
     with open(filename, 'r', encoding="utf-8") as file:
         read_data = file.read()
-        print (read_data, end='')
+        print(read_data, end='')
