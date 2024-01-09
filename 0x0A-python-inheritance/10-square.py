2f835aa052d9a1b69e0f5187e56aa8d1820a0964
kilokomuli <kilokoemma175@gmail.com>
2024-01-09 00:48:58 -0800

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Defines class Square inheriting from class Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ class Square thet inherits from Rectangle"""
+
+    def  __init__(self, size):
+        """ instatiates Square class with size
+        args:
+            size - size of the square"""
+        self.integer_validator = size
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """area method"""
+        return self.__size ** 2
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Defines class Square inheriting from class Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ class Square thet inherits from Rectangle"""
+
+    def  __init__(self, size):
+        """ instatiates Square class with size
+        args:
+            size - size of the square"""
+        self.integer_validator = size
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ Returns a string represntation of the square"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """Returns area of square"""
+        return self.__size ** 2
