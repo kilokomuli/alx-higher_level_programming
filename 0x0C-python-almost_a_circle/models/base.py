b7a42b8f0091778518633b8ff26c7bb99a864bee
kilokomuli <kilokoemma175@gmail.com>
2024-01-15 05:24:47 -0800

    task21

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import json
 import os.path
+import turtle
 """ Defines class Base which will be "base" of all the other classe"""
 
 
@@ -87,3 +88,40 @@ class Base:
             list_ins.append(cls.create(**list_cls[index]))
 
         return list_ins
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw rectangle and square using turtle graphics"""
+
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
+
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
