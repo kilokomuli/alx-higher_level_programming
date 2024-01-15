9d4487c4cc228d852f218c89eaf582afa3b3a3ae
kilokomuli <kilokoemma175@gmail.com>
2024-01-15 01:49:33 -0800

    json-to-string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """ Defines class Base which will be "base" of all the other classe"""
 
 
@@ -18,3 +19,10 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation of list dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
