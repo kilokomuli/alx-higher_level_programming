c3634bedeb390b82217a0d67458ae9d685406eba
kilokomuli <kilokoemma175@gmail.com>
2024-01-15 03:43:49 -0800

    file to instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,7 @@ class Base:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns JSON string representation of list dictionaries"""
         if list_dictionaries is None:
@@ -28,6 +29,7 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+    @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file"""
         filename = "{}.json".format(cls.__name__)
@@ -52,6 +54,7 @@ class Base:
         else:
             return json.loads(json_string)
 
+    @classmethod
     def create(cls, **dictionary):
         """Returns an instance with all attributes
         Args:
@@ -65,3 +68,22 @@ class Base:
         dummy.update(**dictionary)
 
         return(dummy)
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instance"""
+        filename = "{}.json".format(cls.__name__)
+
+        if os.path.exists(filename) is False:
+            return []
+
+        with open(filename, 'r') as f:
+            list_str = f.read()
+
+        list_cls = cls.from_json_string(list_str)
+        list_ins = []
+
+        for index in range(len(list_cls)):
+            list_ins.append(cls.create(**list_cls[index]))
+
+        return list_ins
