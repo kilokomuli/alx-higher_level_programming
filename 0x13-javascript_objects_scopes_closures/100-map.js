9205285d6d420cebb5d0a783e21c2033489cf614
Emma <kilokoemma175@gmail>
2024-03-13 01:17:59 -0700

    advanced

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const originalList = require('./100-data').list;
+console.log(originalList);
+
+const newArray = originalList.map (function (value, index) {
+  return (value * index);
+});
+
+console.log(newArray);
