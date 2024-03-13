583fdb4938e549921ee64e28c56f0c63f6bb0c54
Emma <kilokoemma175@gmail>
2024-03-13 01:20:44 -0700

    correction

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,7 +3,7 @@
 const originalList = require('./100-data').list;
 console.log(originalList);
 
-const newArray = originalList.map (function (value, index) {
+const newArray = originalList.map(function (value, index) {
   return (value * index);
 });
 
