e5906fcc9c68a4ee6498f53039814f32ac979228
Emma <kilokoemma175@gmail.com>
2024-03-15 05:35:21 -0700

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa:
-# that takes 3 arguments:mysql username, mysql password and database name
+""" Lists all states from the database hbtn_0e_0_usa:
+that takes 3 arguments:mysql username, mysql password and database name
+"""
 
 import sys
 import MySQLdb
