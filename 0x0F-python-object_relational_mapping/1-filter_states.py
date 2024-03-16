a4fe4e04ac581fcbad63f102dc8ce362d26befe2
Emma <kilokoemma175@gmail.com>
2024-03-15 22:31:39 -0700

    fix errors1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """ Lists all states with a name starting with N """
-
-
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id LIMIT 2")
-    for row in c.fetchall():
-        print(row)
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    for state in c.fetchall():
+        if state[1][0] == "N":
+            print(state)
+
     c.close()
     db.close()
