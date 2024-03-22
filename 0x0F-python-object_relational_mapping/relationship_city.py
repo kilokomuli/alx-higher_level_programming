7a30e0a7a79bcc5f5008063cae773853b647de19
Emma <kilokoemma175@gmail.com>
2024-03-22 02:51:54 -0700

    reletionships

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class definition of a Ctiy"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """Represents a city for a MySQL database"""
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
