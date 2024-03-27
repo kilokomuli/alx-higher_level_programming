99e23848c0f763c34660c40d7f07bcf7e3c64edb
Emma <kilokoemma175@gmail.com>
2024-03-27 11:02:00 -0700

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a url, sends a request to that URL
-# and displays the size of the body of the response
+# Takes in a url, sends a request to that Url
 curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
