699f42f45a407d4d318d29740c1dac7733799c78
Emma <kilokoemma175@gmail.com>
2024-03-28 10:01:28 -0700

    fixcode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,5 +2,9 @@
 """  script that fetches https://alx-intranet.hbtn.io/status
 using  package urllib """
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+
+
+if __name__ = "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
