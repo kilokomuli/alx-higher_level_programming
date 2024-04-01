fb28c0cd9a5566d73fe1900104cfea33739c01bf
Emma <kilokoemma175@gmail.com>
2024-04-01 11:35:43 -0700

    errorfix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,4 +11,3 @@ if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     result = requests.get("https://api.github.com/user", auth=auth)
     print(result.json().get("id"))
-
