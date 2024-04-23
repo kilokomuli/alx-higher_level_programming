7fafc4d59efeabbaa8129d000f3740eed88bfb97
emma <kilokoemma175@gmail.com>
2024-04-23 00:50:37 -0700

    fs and request

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 
-
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-	if (err) {
-		console.log(err);
-	} else {
-		process.stdout.write(data);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
 });
